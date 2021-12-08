6cbfde9839b4663ca32cf6fda7eb36187d93444a
eks93 <eks93@drexel.edu>
2021-12-07 23:00:34 -0500

    new SVG
    added new svg and started animation

--- deploy/js/scripts-min.js
+++ deploy/js/scripts-min.js
@@ -1,1 +1,25 @@
-import{gsap}from"gsap";import{GSDevTools}from"gsap/GSDevTools";import{DrawSVGPlugin}from"gsap/DrawSVGPlugin";gsap.registerPlugin(GSDevTools,DrawSVGPlugin);const mainTL=gsap.timeline();function drawShape(){return tl.to("#logo",{duration:3,drawSVG:1,delay:.5}),tl}mainTL.add(drawShape()),GSDevTools.create();
+import{gsap}from"gsap";
+import{GSDevTools}from"gsap/GSDevTools";
+import{DrawSVGPlugin}from"gsap/DrawSVGPlugin";
+gsap.registerPlugin(GSDevTools,DrawSVGPlugin);
+
+
+
+const mainTL=gsap.timeline();
+
+
+function drawShape(){
+    const tl = gsap.timeline();
+    tl.to("", {})
+    return tl;
+}
+
+
+
+mainTL.add(drawShape())
+;
+
+
+
+
+GSDevTools.create();
